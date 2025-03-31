745e87a2aa84e231c118cf906781b784e3e34740
A01722540 <gabogal03@gmail.com>
2025-03-30 20:08:58 -0600

    Implementación de base de datos con supabase

--- app/(pages)/dashboard/page.tsx
+++ app/(pages)/dashboard/page.tsx
@@ -1,9 +1,13 @@
+import Test from '@/components/api/test';
+
 export default function Page() {
   return (
     <div>
       <h1>Dashboard</h1>
       <p>This is the dashboard page.</p>
+      <Test/>
     </div>
   );
 }
+
 // This is a simple dashboard page component for a web application.
