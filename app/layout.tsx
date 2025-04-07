e5e000c2d565f882847a2b47becc03d6c4b3230e
Benjamin <benjaminbelloeil@outlook.com>
2025-04-07 15:45:25 -0600

    Add Metadata import to layout component

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { NavigationProvider } from "@/context/navigation-context";
 import { PageTransition } from "@/components/ui/page-transition";
+import { Metadata } from "next";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
